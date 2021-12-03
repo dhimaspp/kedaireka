efc49912127c49de105688cb71e06fca8e2d4579
dhimasputera <dhimasputera@gmail.com>
2021-12-03 18:42:40 +0700

    update sentry

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,23 @@ import 'package:get_storage/get_storage.dart';
 import 'package:kedaireka/theme/constant.dart';
 import 'package:kedaireka/core/auth_manager.dart';
 import 'package:kedaireka/wrapper/auth_validation.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
-  runApp(MyApp());
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://357d1fde2a684565998fc72f32622c88@o883076.ingest.sentry.io/6088650';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(MyApp()),
+  );
+
+  // runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
